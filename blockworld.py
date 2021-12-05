29f090ec7deb4ba4047f7a0a89e9fe3a712ce169
danx0r <dan.miller@eye0.com>
2021-12-04 19:35:35 -0800

    wicked

--- blockworld.py
+++ blockworld.py
@@ -1,47 +1,73 @@
 import sys
 
-bugg = 0
-def parse(toks, i=0, end=(".",)):
-    global bugg
-    bugg += 1
-    print ("   recursion level:", bugg, "i:", i, "toks:", toks, "end:", end)
-    out = []
-    ender = None
-    while i < len(toks):
-        tok = toks[i]
-        if tok in end:
-            ender = tok
-            break
-        if tok == "put":
-            nu, i, ender = parse(toks, i + 1, ('in', 'on'))
-            out += nu
-            out.append(f"put-{ender}")
-        else:
-            out.append(tok)
-        print ("==========>", tok, out)
-        i += 1
-    bugg -= 1
-    print ("   returns:", out, i, ender)
-    return out, i, ender
+STACK = []
+TOKENS = []
+INDEX = 0
+
+TABLE = []
+BASKET = ["block1_red", "block2_blue"]
+
+def process_token():
+    global INDEX
+    token = TOKENS[INDEX]
+    INDEX += 1
+    funcname = f"_{token}"
+    if funcname in globals():
+        print ("CALL", funcname)
+        return globals()[funcname]()
+    else:
+        print ("SKIP:", token)
+        return token
+
+def tokenize_sentence(s):
+    for sep in ".,?:;":
+        s = s.replace(sep, " %s " % sep)
+    s = s.strip().lower()
+    s = s.split()
+    return s
+
+
+def parse_sentence(s):
+    global INDEX
+    t = tokenize_sentence(s)
+    INDEX = 0
+    TOKENS.clear()
+
+    for tok in t:
+        TOKENS.append(tok)
+    print(TOKENS)
+    while INDEX < len(TOKENS):
+        process_token()
+
+
+def _put():
+    subj = process_token()
+    prep = process_token()
+    obj = process_token()
+    print ("PUT:", subj, prep, obj)
+    if subj == 'block':
+        if obj == 'table':
+            TABLE.append(BASKET.pop())
+        elif obj == 'basket':
+            BASKET.append(TABLE.pop())
+
+
+def _and():
+    return process_token()
+
+
+def _a():
+    return process_token()
+
+
+def _the():
+    return process_token()
 
 
 if __name__ == "__main__":
-    fn = sys.argv[1]
-
-    f = open(fn)
-
-    for row in f.readlines():
-        for sep in ".,?:;":
-            row = row.replace(sep, " %s " % sep)
-        row = row.strip().lower()
-        if not row:
-            continue
-        row = row.split()
-        for dis in ('the', 'a'):
-            while dis in row:
-                row.remove(dis)
-        print ("---------------------------------------")
-        print (row)
-        out = parse(row)
-        print (out)
-        break
+    print("BASKET:", BASKET)
+    print("TABLE:", TABLE)
+    print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    parse_sentence("Put a block on the table and put a block on the table Bob.")
+    print("BASKET:", BASKET)
+    print("TABLE:", TABLE)
